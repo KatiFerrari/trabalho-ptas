08c040f74ce427864136383e4ab6e0d65f74010d
KatiFerrari <katielidasilvaferrari26@gmail.com>
2023-09-21 19:55:00 -0400

    botão remover

--- src/Todo/ToDo.jsx
+++ src/Todo/ToDo.jsx
@@ -2,69 +2,77 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./style.css"
 
+
 export default function ToDo() {
    const [nome, setNome ] = useState("");
    const [marca, setMarca ] = useState("");
    const [preco, setPreco ] = useState("");
-   const [imagem, setImagem ] = useState("");
+   const [img, setImg] = useState("");
    const [lista, setLista ] = useState([]);
    const [id,setId] = useState(1);
     const salvar =(e) =>{
         e.preventDefault();
         setLista([...lista, {
-                nome:nome, marca:marca, preço:preco, imagem: imagem,
+                nome: nome, marca:marca, preco:preco, img:img,
+
                 id: id
         }]);
         setId(id + 1);
         setNome("");
         setMarca("");
         setPreco("");
-        setImagem("");
-        
+        setImg("");
+        console.log(id)
     };
+
+    const remover = (id) => {
+        const auxLista = [];
+        lista.map((lista) => {
+            if(lista.id !== id){
+                auxLista.push(lista);
+            }
+        });
+        setLista(auxLista);
+    }
 
     return (
         <div>
             <Link to="/">home</Link>
-            <h1>Lista de Produtos</h1>    
+            <h1>Adicionar óculos em sua coleção</h1>  
+            <div classname="flex.conteiner"> 
             <form onSubmit={salvar}>
-            
-            <p>Nome:</p>
-            &emsp;
-           <input value={nome} type="text"
-            
+                <p><strong>Nome:</strong></p>
+                &emsp;
+            <input value={nome} type="text"
             onChange={(e)=>{ setNome(e.target.value)}}/>
-
-            <p>Marca:</p>
-            &emsp;
+             <p><strong>Marca:</strong></p>
+          &emsp;
             <input value={marca} type="text"
             onChange={(e)=>{ setMarca(e.target.value)}}/>
-            
-           <p>Preço:</p>
+            <p><strong>Preço:</strong></p>
            &emsp;
-           <input value={preco} type="text"
+            <input value={preco} type="text"
             onChange={(e)=>{ setPreco(e.target.value)}}/>
-            
-            <p>Imagem:</p>
-           &emsp;
-           <input value={imagem} type="text"
-            onChange={(e)=>{ setImagem(e.target.value)}}/>
-
+            <p><strong>URL da Imagem:</strong></p>
             &emsp;
-            <button  class="btn btn-dark">ADD</button>
-
-            </form>      
+            <input value={img} type="text"
+            onChange={(e)=>{ setImg(e.target.value)}}/>
+            &emsp;
+            <button className="btn btn-dark">ADD</button>
+            </form>   
+            </div>    
             {lista.map((ativ)=>
             <div key= {ativ.id}>
-                <p>Nome: {ativ.nome}</p>
-                <p>Marca: {ativ.marca}</p>
-                <p>Preço: R${ativ.preco}</p>
-                <p>Imagem:</p>
-                <img src={ativ.imagem} alt="" />
-
+                <p><strong>Tipo:</strong>{ativ.tipo}</p>
+                <p><strong>Marca:</strong>{ativ.marca}</p>
+                <p><strong>Preço:</strong>R${ativ.preco}</p>
+                <p><strong>Imagem:</strong></p>
+                <img src={ativ.img} alt="" />
+                <br />
+                <br />
+                <button class="btn btn-outline-primary"onClick={() => remover(ativ.id)}>Remover</button>
             </div>
             )} 
         </div>
     );
 }
-
